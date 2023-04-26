71e992243c57ab8fb62bf4b4186bd9c44b3411e9
KiraTeresa <kira.soba@gmx.de>
2023-04-26 16:38:55 +0200

    finding web elements

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -38,6 +38,36 @@ describe("Our first suite", () => {
 
   })
 
+  it.only('second test', () => {
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+    cy.get('[data-cy="signInButton"')
+
+    // finds first
+    cy.contains('Sign in')
+
+    // adding a unique identifier
+    cy.contains('[status="warning"]','Sign in')
+
+    // find sign in button through another element of the same component by "travelling up"
+    // find is used, to find the child element within a parent element, therefore a parent command must be run first
+    // get would give us all buttons which are nested in a form
+    cy.get('#inputEmail3')
+      .parents('form')
+      .find('button')
+      .should('contain', 'Sign in')
+      .parents('form')
+      .find('nb-checkbox')
+      .click()
+
+    // find the element "nb-card", which contains "Horizontal form"
+    // within this element, find the one with the attribute "type", whos value is "email"
+    cy.contains('nb-card','Horizontal form').find('[type="email"]')
+
+  })
+
 })
 
 
