f8a24c22d725db57285525324d7ddd9187dca75e
KiraTeresa <kira.soba@gmx.de>
2023-04-27 11:23:36 +0200

    invoke command

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -78,13 +78,12 @@ describe("Our first suite", () => {
 
   })
 
-
   /*
 **********************************
 * Lesson 24: Saving Subject of the Command
 **********************************
 */
-  it.only("then and wrap methods", () => {
+  it("then and wrap methods", () => {
     cy.visit('/')
     cy.contains('Forms').click()
     cy.contains('Form Layouts').click()
@@ -113,6 +112,52 @@ describe("Our first suite", () => {
       })
     })
 
+  })
+
+  /*
+  **********************************
+  * Lesson 25: Invoke Command
+  **********************************
+  */
+  it('invoke command', () => {
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+    // 1
+    cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+    // 2
+    cy.get('[for="exampleInputEmail1"]').then(label => {
+      expect(label.text()).to.equal('Email address')
+    })
+
+    // 3
+    cy.get('[for="exampleInputEmail1"]').invoke('text').then(text => {
+      expect(text).to.equal('Email address')
+    })
+
+    cy.contains('nb-card', 'Basic form')
+      .find('nb-checkbox')
+      .click()
+      .find('.custom-checkbox')
+      .invoke('attr', 'class')
+      // .should('contain', 'checked')
+      .then(classValue => {
+        expect(classValue).to.contain('checked')
+      })
+  })
+
+  it.only('assert property', () => {
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Datepicker').click()
+
+    cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
+      cy.wrap(input).click()
+      cy.get('nb-calendar-day-picker').contains('17').click()
+      cy.wrap(input).invoke('prop', 'value').should('contain', 'Apr 17, 2023')
+    })
   })
 })
 
